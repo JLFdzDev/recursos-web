a28e980b2cd2dff47993faf3dbccf3bf11107dad
JLFdzDev <jlfdzdev@gmail.com>
2024-04-06 19:54:25 +0200

    Add tag filter

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -1,9 +1,19 @@
 import { prisma } from '@/database/connection'
+import { NextRequest } from 'next/server'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+	const tag = req.nextUrl.searchParams.get('tag')
+	const tagFilter = tag != null && { tags: { some: { name: tag } } }
+
 	const data = await prisma.resource.findMany({
+		where: {
+			...tagFilter,
+		},
 		include: {
 			tags: true,
+		},
+		orderBy: {
+			name: 'asc',
 		},
 	})
 
