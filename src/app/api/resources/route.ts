2d59092ffc0976961b8de52fe1e951932fc7c3aa
JLFdzDev <jlfdzdev@gmail.com>
2024-04-08 16:26:14 +0200

    Add POST METHOD to create new resource

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/database/connection'
+import { ResourceInputs } from '@/types'
 import { NextRequest } from 'next/server'
 
 export async function GET(req: NextRequest) {
@@ -14,6 +15,27 @@ export async function GET(req: NextRequest) {
 		},
 		orderBy: {
 			name: 'asc',
+		},
+	})
+
+	return Response.json(data)
+}
+
+export async function POST(req: NextRequest) {
+	const { name, url, poster, tags, description }: ResourceInputs = await req.json()
+
+	const data = await prisma.resource.create({
+		data: {
+			name,
+			url,
+			poster,
+			description,
+			tags: {
+				connect: tags.map((tag) => ({ id: tag.value })),
+			},
+		},
+		include: {
+			tags: true,
 		},
 	})
 
