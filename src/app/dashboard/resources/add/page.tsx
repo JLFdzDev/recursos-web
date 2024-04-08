372c85418e6f2c427a350162363ce79c51ac12a9
JLFdzDev <jlfdzdev@gmail.com>
2024-04-08 16:27:13 +0200

    Update form to create new resource using react-hook-form

--- src/app/dashboard/resources/add/page.tsx
+++ src/app/dashboard/resources/add/page.tsx
@@ -5,8 +5,6 @@ import { ResourceForm } from '@/app/dashboard/resources/components/form'
 import { getData } from '@/utils'
 
 export default async function DashboardResourcesAddPage() {
-	console.log(`${process.env.API_URL}/tags`)
-
 	const tags = await getData<Tag[]>(`${process.env.API_URL}/tags`, { cache: 'no-store' })
 	const selectOptions = tags.map((tag) => ({
 		value: tag.id,
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,13 +1,43 @@
 import { Resource as ResourceBase } from '@prisma/client'
 import { Tag as TagBase } from '@prisma/client'
 
+/*
+ *********************************************
+ * MODELS
+ *********************************************
+ */
 export interface Resource extends ResourceBase {
 	tags: Tag[]
 }
 
 export interface Tag extends TagBase {}
 
+/*
+ *********************************************
+ * END MODELS
+ *********************************************
+ */
+
+/*
+ *********************************************
+ * FORMS
+ *********************************************
+ */
 export interface SelectOption {
 	value: string
 	label: string
 }
+
+export interface ResourceInputs {
+	name: string
+	url: string
+	poster: string
+	tags: SelectOption[]
+	description: string
+}
+
+/*
+ *********************************************
+ * END FORMS
+ *********************************************
+ */
