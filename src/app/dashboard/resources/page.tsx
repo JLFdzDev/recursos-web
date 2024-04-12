660e508fdddf9e4094f3f29ea7daea94f4b94af8
JLFdzDev <jlfdzdev@gmail.com>
2024-04-12 16:28:57 +0200

    Fix no revalidate page

--- src/app/dashboard/resources/page.tsx
+++ src/app/dashboard/resources/page.tsx
@@ -2,6 +2,7 @@ import { INPUT_BASE_CLASSNAME } from '@/const/theme'
 
 import { List } from '@/app/dashboard/resources/components/list'
 import { ButtonLink } from '@/components/button'
+import { Suspense } from 'react'
 
 export default function DashboardResourcesPage() {
 	return (
@@ -10,7 +11,9 @@ export default function DashboardResourcesPage() {
 				<input type="text" placeholder="Search..." className={`${INPUT_BASE_CLASSNAME} flex-1`} />
 				<ButtonLink href="/dashboard/resources/add">Nuevo</ButtonLink>
 			</div>
-			<List />
+			<Suspense fallback={<h1>Cargando</h1>}>
+				<List />
+			</Suspense>
 		</>
 	)
 }
