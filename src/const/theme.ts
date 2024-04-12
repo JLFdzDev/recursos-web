1698d7beeee331c5264f08e3f14efa2db8125bf7
JLFdzDev <jlfdzdev@gmail.com>
2024-04-12 16:27:08 +0200

    Update multiselect theme

--- src/const/theme.ts
+++ src/const/theme.ts
@@ -8,7 +8,7 @@ export const REACT_SELECT_BASE_CLASSNAME = {
 		'bg-primary text-light rounded-r-full text-sm px-1 py-0.5 hover:bg-secondary hover:text-primary transition-colors duration-300',
 	menu: () => 'bg-dark mt-1 border-darken border-2 rounded-lg overflow-x-hidden overflow-y-auto z-10',
 	option: () =>
-		'px-4 py-2 hover:bg-primary/50 hover:text-primary transition-colors duration-300 border-b-2 border-darken last:border-b-0',
+		'px-4 py-2 hover:bg-primary/50 hover:text-primary transition-colors duration-300 border-b-2 border-darken last:border-b-0 aria-selected:bg-primary/50',
 	indicatorsContainer: () => 'flex justify-between gap-2',
 	indicatorSeparator: () => 'bg-darken',
 	clearIndicator: () => 'text-secondary',
