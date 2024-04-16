33539a4f661cf935f3f10b10518617d58aae0a0f
JLFdzDev <jlfdzdev@gmail.com>
2024-04-16 14:40:34 +0200

    Apply <Form /> to tags

--- src/app/dashboard/tags/components/form.tsx
+++ src/app/dashboard/tags/components/form.tsx
@@ -1,9 +1,9 @@
 import { INPUT_BASE_CLASSNAME } from '@/const/theme'
 
-import { Button, ButtonLink } from '@/components/button'
+import { Form } from '@/app/dashboard/components/form'
 
-import { addTag, updateTag } from '@/lib/actions/tags'
 import { findById } from '@/db/repository/tags'
+import { addTag, updateTag } from '@/lib/actions/tags'
 
 interface TagFormProps {
 	id?: string
@@ -11,27 +11,17 @@ interface TagFormProps {
 
 export const TagForm = async ({ id }: TagFormProps) => {
 	const tag = id ? await findById(id) : null
-	const action = id ? await updateTag : await addTag
 
 	return (
-		<>
-			<form action={action} className="grid grid-cols-1">
-				{id != null && <input type="hidden" name="id" value={id} />}
-				<input
-					type="text"
-					name="name"
-					className={`${INPUT_BASE_CLASSNAME} mb-2`}
-					placeholder="Nombre (*)"
-					required
-					defaultValue={tag?.name}
-				/>
-				<div className="col-span-2 flex justify-center gap-4">
-					<ButtonLink href="/dashboard/tags">Atrás</ButtonLink>
-					<Button type="submit" variant="secondary">
-						Guardar
-					</Button>
-				</div>
-			</form>
-		</>
+		<Form id={id} backRoute="/dashboard/tags" addAction={addTag} updateAction={updateTag}>
+			<input
+				type="text"
+				name="name"
+				className={`${INPUT_BASE_CLASSNAME} mb-2`}
+				placeholder="Nombre (*)"
+				required
+				defaultValue={tag?.name}
+			/>
+		</Form>
 	)
 }
