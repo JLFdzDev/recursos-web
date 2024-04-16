5711ee9b0625aa3a44a675bb39ff0e86e4929cb4
JLFdzDev <jlfdzdev@gmail.com>
2024-04-16 16:02:33 +0200

    Update form to accept className as prop

--- src/app/dashboard/components/form.tsx
+++ src/app/dashboard/components/form.tsx
@@ -3,15 +3,23 @@ import { PropsWithChildren } from 'react'
 import { Button, ButtonLink } from '@/components/button'
 
 interface FormProps extends PropsWithChildren {
+	className?: string
 	id?: string
 	backRoute: string
 	addAction?: (formData: FormData) => void
 	updateAction?: (formData: FormData) => void
 }
 
-export const Form = async ({ backRoute, children, id, addAction, updateAction }: FormProps) => {
+export const Form = async ({
+	backRoute,
+	className = 'grid grid-cols-2 gap-2',
+	children,
+	id,
+	addAction,
+	updateAction,
+}: FormProps) => {
 	return (
-		<form action={id != null ? await updateAction : await addAction} className="grid grid-cols-1">
+		<form action={id != null ? await updateAction : await addAction} className={className}>
 			{id != null && <input type="hidden" name="id" value={id} />}
 			{children}
 			<div className="col-span-2 flex justify-center gap-4">
