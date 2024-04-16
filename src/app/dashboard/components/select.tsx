fe6b4638069bcc46b38a849be47cae3f9be53752
JLFdzDev <jlfdzdev@gmail.com>
2024-04-16 16:03:11 +0200

    Create Select component with 'use client' directive

---
+++ src/app/dashboard/components/select.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { SelectOption } from '@/types'
+
+import ReactSelect from 'react-select'
+
+import { REACT_SELECT_BASE_CLASSNAME } from '@/const/theme'
+
+interface SelectProps {
+	defaultValue?: SelectOption[]
+	isMulti?: boolean
+	name: string
+	options: SelectOption[]
+	placeholder?: string
+}
+
+export const Select = ({
+	defaultValue,
+	isMulti = false,
+	name,
+	options,
+	placeholder = 'Selecciona uno...',
+}: SelectProps) => {
+	return (
+		<ReactSelect
+			name={name}
+			options={options}
+			classNames={REACT_SELECT_BASE_CLASSNAME}
+			placeholder={placeholder}
+			unstyled
+			closeMenuOnSelect={false}
+			hideSelectedOptions={false}
+			isMulti={isMulti}
+			instanceId={name}
+			defaultValue={defaultValue}
+		/>
+	)
+}
