7a74b238991c742e18a12c9817fe9d758bfe1b4b
JLFdzDev <jlfdzdev@gmail.com>
2024-05-16 16:42:24 +0200

    Added navbar to homepage when user exist in session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from '@/app/components/header'
 import { ResourcesList } from '@/app/components/resources-list'
+import { Navbar } from '@/app/dashboard/components/navbar'
+import { Pagination } from '@/components/pagination'
+import { Suspense } from 'react'
 
 import { count } from '@/db/repository/resources'
 
-import { Pagination } from '@/components/pagination'
+import { auth } from '@/auth'
 import { getLimitFilter, getTotalPages } from '@/utils'
-import { Suspense } from 'react'
 
 interface HomeProps {
 	searchParams: {
@@ -15,11 +17,19 @@ interface HomeProps {
 }
 
 export default async function Home({ searchParams: { tag, page = '1' } }: HomeProps) {
+	const session = await auth()
+
 	const totalPages = getTotalPages(await count())
 	const limit = getLimitFilter(page)
 
 	return (
 		<main className="w-full max-w-screen-xl mx-auto py-10">
+			{session != null && (
+				<>
+					<Navbar />
+					<div className="mb-6" />
+				</>
+			)}
 			<Header currentFilter={tag} />
 			<Suspense fallback={<h1>Cargando</h1>}>
 				<ResourcesList tagFilter={tag} limit={limit} />
